eaf7f91881cbfbe8ebe997b38b4b40785e48b7d5
Henrique <henriquetx07@gmail.com>
2023-10-17 03:32:46 -0300

    Adição de botões para Editar e Excluir tasks

--- App.py
+++ App.py
@@ -6,7 +6,7 @@ import PySimpleGUI as sg
 layout = [
     [sg.Text("Lista de Tarefas")],
     [sg.Listbox([], size=(40, 10), key="-LIST-")],
-    [sg.Button("Nova Tarefa"), sg.Button("Sair")]
+    [sg.Button("Nova Tarefa"),sg.Button("Editar tarefa"),sg.Button("Excluir Tarefa"),sg.Button("Sair")]
 ]
 #Criar a janela
 window = sg.Window("Gerenciador de Tarefas", layout)
@@ -22,7 +22,7 @@ while True:
     if event == sg.WIN_CLOSED or event == "Sair":
         break
 
-    # Se o usuário clicar no botão "Nova Tarefa", abra a nova janela de tarefa
+    # Função do botão "Nova Tarefa"
     if event == "Nova Tarefa":
         # Defina o layout da janela de tarefa
         task_layout = [
@@ -30,26 +30,67 @@ while True:
             [sg.Button("Salvar"), sg.Button("Excluir")]
         ]
 
-        # Crie a janela de tarefa
+        #  ---------------- Crie a janela de tarefa------------------
         task_window = sg.Window("Nova Tarefa", task_layout)
 
         while True:
-            event, values = task_window.read()
+            event_task, values_task = task_window.read()
 
             # Se o usuário fechar a janela de tarefa, saia do loop interno
-            if event == sg.WIN_CLOSED:
+            if event_task == sg.WIN_CLOSED or event_task == "Cancelar":
                 break
 
             # Se o usuário clicar em "Salvar", adicione a tarefa à lista e saia
-            if event == "Salvar":
-                task_text = values["task"]
+            if event_task == "Salvar":
+                task_text = values_task["task"]
                 tasks.append(task_text)
                 task_window.close()
-                window["-LIST-"].update(values=tasks)
+                task_display = [f"Tarefa {i + 1}: {task}" for i, task in enumerate(tasks)]
+                window["-LIST-"].update(values=task_display)
                 break
 
-            # Se o usuário clicar em "Excluir", saia da janela de tarefa
-            if event == "Excluir":
-                task_window.close()
+        task_window.close()
+
 # Feche a janela principal
+window.close()
+
+#Função do botão "Editar Tarefa"
+if event=="Editar Tarefa":
+    selected_task_index=values["-LIST-"][0]
+    if selected_task_index :
+        selected_task_index = int(selected_task_index[0])
+        task_layout= [
+            [sg.Multiline(tasks[selected_task_index],size=(40,10), key="task")],
+            [sg.Button("Salvar"), sg.Button("Cancelar")]
+        ]
+        task_window=sg.Window("Editar Tarefa", task_layout)
+
+        while True:
+            event_task, values_tarefa=task_window.read()
+
+            # Se o usuário fechar a janela de tarefa, saia do loop interno
+            if event_task == sg.WIN_CLOSED or event_task == "Cancelar":
+                break
+            
+            #"Salvar" atualaiza na lista
+            if event_task == "Salvar":
+                task_text=values_task["task"]
+                tasks[selected_task_index]=task_text
+                task_window.close()
+                task_display = [f"Tarefa {i + 1}: {task}" for i, task in enumerate(tasks)]
+                window["-LIST-"].update(values=task_display)
+                break
+
+        task_window.close()
+
+
+#Adição da função Excluir Tarefa
+if event=="Excluir Tarefa":
+    selected_task_index=values["-LIST-"][0]
+    if selected_task_index:
+        selected_task_index = int(selected_task_index[0])
+        tasks.pop(selected_task_index)
+        task_display = [f"Tarefa {i + 1}: {task}" for i, task in enumerate(tasks)]
+        window["-LIST-"].update(values=task_display)
+
 window.close()
